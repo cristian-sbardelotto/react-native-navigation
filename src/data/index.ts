c62973515f829d7f71b39ab8648b09a313bc22d2
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2023-03-30 16:16:00 -0300

    feat(data): created the folder and added the mock file

---
+++ src/data/index.ts
@@ -0,0 +1,32 @@
+export const contactsList = [
+  {
+    id: 0,
+    name: 'Cristian',
+    bio: 'Hello! My Name is Cristian',
+    number: '+ 55 51 123456789',
+  },
+  {
+    id: 1,
+    name: 'John',
+    bio: 'Hello! My Name is John',
+    number: '+ 55 51 123456789',
+  },
+  {
+    id: 2,
+    name: 'Marta',
+    bio: 'Hello! My Name is Marta',
+    number: '+ 55 51 123456789',
+  },
+  {
+    id: 3,
+    name: 'Joseph',
+    bio: 'Hello! My Name is Joseph',
+    number: '+ 55 51 123456789',
+  },
+  {
+    id: 4,
+    name: 'Frisk',
+    bio: 'Hello! My Name is Frisk',
+    number: '+ 55 51 123456789',
+  },
+];
